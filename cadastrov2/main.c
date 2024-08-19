fd86da8362637e9976c54078680d44bec3a1bfd9
lucascript1 <166554906+lucascript1@users.noreply.github.com>
2024-08-19 16:00:53 -0300

    main.c

--- cadastrov2/main.c
+++ cadastrov2/main.c
@@ -21,10 +21,26 @@ int mostra_menu()
     printf("3 - Pesquisar.:... \n");
     printf("4 - Gravar.:... \n");
     printf("5 - Ler arquivo:... \n");
+    printf("6 - Ordenar produtos:...\n");
     printf("0 - Sair.:..... \n");
     printf("Tecle a opcao:  \n");
     scanf("%d", &opcao);
     return opcao;
+}
+
+void ordenar_produtos(tProd* vProd, int qtd) {
+    int i, j;
+    tProd temp;
+    for (i = 0; i < qtd - 1; i++) {
+        for (j = i + 1; j < qtd; j++) {
+            if (strcmp(vProd[i].descricao, vProd[j].descricao) > 0) {
+                temp = vProd[i];
+                vProd[i] = vProd[j];
+                vProd[j] = temp;
+            }
+        }
+    }
+    listar(vProd, qtd);
 }
 
 int inclusao(tProd* vProd, int qtd)
@@ -32,20 +48,20 @@ int inclusao(tProd* vProd, int qtd)
     system("cls");
     printf("Inclusao\n");
 
-    // codigo manual
-    //printf("codigo....: ");
-    //scanf("%d", &vProd[qtd].codigo);
     // codigo automatico
     vProd[qtd].codigo = qtd + 1;
 
     printf("descricao.: ");
-    fflush(stdin);
-    gets(vProd[qtd].descricao);
+    getchar(); // para consumir o '\n' deixado pelo scanf anterior
+    fgets(vProd[qtd].descricao, sizeof(vProd[qtd].descricao), stdin);
+    vProd[qtd].descricao[strcspn(vProd[qtd].descricao, "\n")] = '\0'; // Remove o '\n'
+
     printf("preco.....: ");
     scanf("%f", &vProd[qtd].preco);
+
     printf("Registro Incluido!\n");
     system("pause");
-    return qtd+1;
+    return qtd + 1;
 }
 
 int listar(tProd* vProd, int qtd)
@@ -55,7 +71,7 @@ int listar(tProd* vProd, int qtd)
     printf("+-------------------------------------------------+\n");
     printf("|              Listagem de Produtos               |\n");
     printf("+-------------------------------------------------+\n");
-    for (i=0; i<qtd; i++)
+    for (i = 0; i < qtd; i++)
     {
         printf("| %5d | %-30s | %6.2f |\n", vProd[i].codigo, vProd[i].descricao, vProd[i].preco);
     }
@@ -63,12 +79,13 @@ int listar(tProd* vProd, int qtd)
     printf("| Total de Registros: %3d                         |\n", qtd);
     printf("+-------------------------------------------------+\n");
     system("pause");
+    return qtd;
 }
 
 void print_spaces(int n)
 {
     int i;
-    for (i=0; i<n; i++)
+    for (i = 0; i < n; i++)
     {
         printf(" ");
     }
@@ -76,95 +93,95 @@ void print_spaces(int n)
 
 int pesquisar(tProd* vProd, int qtd)
 {
-    int i, cont=0;;
+    int i, cont = 0;
     char buscar[50];
     system("cls");
     printf("Informe a Expressao de Busca: ");
-    fflush(stdin);
-    gets(buscar);
+    getchar(); // para consumir o '\n' deixado pelo scanf anterior
+    fgets(buscar, sizeof(buscar), stdin);
+    buscar[strcspn(buscar, "\n")] = '\0'; // Remove o '\n'
 
     printf("+-------------------------------------------------+\n");
-    printf("| Pesquisando Produto '%s'",buscar);
-    print_spaces(26-strlen(buscar));
+    printf("| Pesquisando Produto '%s'", buscar);
+    print_spaces(26 - strlen(buscar));
     printf("|\n");
     printf("+-------------------------------------------------+\n");
-    for (i=0; i<qtd; i++)
+    for (i = 0; i < qtd; i++)
     {
-        if (strstr(strlwr(vProd[i].descricao), strlwr(buscar))!=NULL)
+        if (strstr(vProd[i].descricao, buscar) != NULL)
         {
             printf("| %5d | %-30s | %6.2f |\n", vProd[i].codigo, vProd[i].descricao, vProd[i].preco);
             cont++;
-        },
+        }
     }
     printf("+-------------------------------------------------+\n");
     printf("| %3d Registros Encontrados                       |\n", cont);
     printf("+-------------------------------------------------+\n");
     system("pause");
+    return cont;
 }
+
 int gravar(tProd* vProd, int qtd)
 {
     int i;
     char nomearquivo[] = "produtos.csv";
     FILE* file = fopen(nomearquivo, "w");
-    if(file == NULL)
+    if (file == NULL)
     {
         printf("Erro ao tentar gravar o arquivo\n");
     }
     else
     {
-        for (i=0; i<qtd; i++)
+        for (i = 0; i < qtd; i++)
         {
-            fprintf(file, "%d;%s;%f\n", vProd[i].codigo, vProd[i].descricao, vProd[i]. preco);
+            fprintf(file, "%d;%s;%f\n", vProd[i].codigo, vProd[i].descricao, vProd[i].preco);
         }
         printf("Arquivo gravado com sucesso!\n");
         system("pause");
     }
     fclose(file);
+    return qtd;
 }
+
 int ler_arquivo(tProd* vProd)
 {
-    int contador =0;
-
-    char linha[256] = "";
+    int cont = 0;
+    char linha[256];
     char* token;
     char nomearquivo[] = "produtos.csv";
     FILE* file = fopen(nomearquivo, "r");
-    if(file == NULL)
+    if (file == NULL)
     {
         printf("Erro ao ler o arquivo\n");
     }
     else
     {
-        //ler linhas
-        while (fgets(linha,256, file))
+        while (fgets(linha, sizeof(linha), file))
         {
-            printf("%s", linha);
-            //processar os campos da linha
-            //codigo
             token = strtok(linha, ";\n");
-            vProd[contador].codigo = atoi(token);
-            //descricao
-            token = strtok(NULL, ";\n");
-            strcpy(vProd[contador].descricao, token);
-            //preco
-            token = strtok(linha, ";\n");
-            vProd[contador].preco = atof(token);
-            contador ++;
+            if (token != NULL) {
+                vProd[cont].codigo = atoi(token);
+                token = strtok(NULL, ";\n");
+                if (token != NULL) {
+                    strcpy(vProd[cont].descricao, token);
+                    token = strtok(NULL, ";\n");
+                    if (token != NULL) {
+                        vProd[cont].preco = atof(token);
+                        cont++;
+                    }
+                }
             }
-
-           printf("%d registros lidos!\n", contador);
-
         }
+        printf("%d registros lidos!\n", cont);
         fclose(file);
         system("pause");
-    return contador;
     }
-
-
+    return cont;
+}
 
 int main()
 {
-    tProd vProd[3];
+    tProd vProd[100];
     int qtd = 0;
     int opcao;
 
@@ -187,7 +204,10 @@ int main()
             gravar(vProd, qtd);
             break;
         case 5:
-            ler_arquivo(vProd);
+            qtd = ler_arquivo(vProd);
+            break;
+        case 6:
+            ordenar_produtos(vProd, qtd);
             break;
         case 0:
             printf("Encerrando...\n");
@@ -197,6 +217,7 @@ int main()
             system("pause");
         }
 
-    }
-    while (opcao != 0);
+    } while (opcao != 0);
+
+    return 0;
 }
